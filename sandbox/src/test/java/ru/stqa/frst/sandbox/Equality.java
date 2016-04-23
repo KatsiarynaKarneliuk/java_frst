0f326e1f17f028cb504984c07e55b5e71fbb08e5
KatsiarynaKarneliuk <udivitelniymir@mail.ru>
2016-04-23 22:50:11 +0300

    Исправлено == на equals

---
+++ sandbox/src/test/java/ru/stqa/frst/sandbox/Equality.java
@@ -0,0 +1,12 @@
+package ru.stqa.frst.sandbox;
+
+/**
+ * Created by user on 23.04.2016.
+ */
+public class Equality {
+  public static void main(String[]args) {
+    String s1 = "firefox";
+    String s2 = s1;
+    
+  }
+}
