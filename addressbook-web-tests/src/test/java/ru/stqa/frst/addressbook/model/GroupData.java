0e58b03e202900ea06d14da123083a436aabb217
KatsiarynaKarneliuk <udivitelniymir@mail.ru>
2016-05-02 14:08:41 +0300

    Сравнение коллекций целиком

--- addressbook-web-tests/src/test/java/ru/stqa/frst/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/frst/addressbook/model/GroupData.java
@@ -5,6 +5,29 @@ public class GroupData {
   private final String header;
   private final String name;
 
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "name='" + name + '\'' +
+            '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    return name != null ? name.equals(groupData.name) : groupData.name == null;
+
+  }
+
+  @Override
+  public int hashCode() {
+    return name != null ? name.hashCode() : 0;
+  }
+
   public GroupData(String footer, String header, String name) {
     this.footer = footer;
     this.header = header;
--- addressbook-web-tests/src/test/java/ru/stqa/frst/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/frst/addressbook/tests/GroupDeletionTests.java
@@ -9,20 +9,25 @@ import java.util.List;
 public class GroupDeletionTests extends TestBase {
 
 
-    @Test
-    public void testGroupDeletion() {
+  @Test
+  public void testGroupDeletion() {
 
-        app.gotoGroupPage();
-        if (!app.getGroupHelper().isThereAGroup()){
-            app.getGroupHelper().createGroup(new GroupData("test1","test2",null));
-        }
-        List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().selectGroup(before.size() - 1);
-        app.getGroupHelper().deleteSelectedGroups();
-        app.getGroupHelper().returnToGroupPage();
-        List<GroupData>  after = app.getGroupHelper().getGroupList();
-        Assert.assertEquals(after.size(), before.size() - 1);
+    app.gotoGroupPage();
+    if (!app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", "test2", null));
     }
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    app.getGroupHelper().selectGroup(before.size() - 1);
+    app.getGroupHelper().deleteSelectedGroups();
+    app.getGroupHelper().returnToGroupPage();
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+
+
+    Assert.assertEquals(before, after);
+  }
 
 }
 
