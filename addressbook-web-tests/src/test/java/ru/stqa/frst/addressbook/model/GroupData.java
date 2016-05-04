18e205510ceeb89bb4f5366bc611142d4050cfb2
KatsiarynaKarneliuk <udivitelniymir@mail.ru>
2016-05-04 21:50:46 +0300

    Поиск максимального элемента в коллекции (GroupCreationTests)

--- addressbook-web-tests/src/test/java/ru/stqa/frst/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/frst/addressbook/model/GroupData.java
@@ -1,50 +1,26 @@
 package ru.stqa.frst.addressbook.model;
 
 public class GroupData {
-  private final String id;
+  private int id;
   private final String footer;
   private final String header;
   private final String name;
 
-  public String getId() {
+  public int getId() {
     return id;
   }
 
 
-  @Override
-  public String toString() {
-    return "GroupData{" +
-            "id='" + id + '\'' +
-            ", name='" + name + '\'' +
-            '}';
-  }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
 
-    GroupData groupData = (GroupData) o;
-
-    if (id != null ? !id.equals(groupData.id) : groupData.id != null) return false;
-    return name != null ? name.equals(groupData.name) : groupData.name == null;
-
-  }
-
-  @Override
-  public int hashCode() {
-    int result = id != null ? id.hashCode() : 0;
-    result = 31 * result + (name != null ? name.hashCode() : 0);
-    return result;
-  }
   public GroupData(String name, String header, String footer) {
-    this.id = null;
+    this.id = 0;
     this.footer = footer;
     this.header = header;
     this.name = name;
   }
 
-  public GroupData(String id, String name, String header, String footer) {
+  public GroupData(int id, String name, String header, String footer) {
     this.id = id;
     this.footer = footer;
     this.header = header;
@@ -61,5 +37,36 @@ public class GroupData {
 
   public String getName() {
     return name;
+  }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    if (id != groupData.id) return false;
+    return name != null ? name.equals(groupData.name) : groupData.name == null;
+
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    return result;
+  }
+
+  @Override
+
+  public String toString() {
+    return "GroupData{" +
+            "id='" + id + '\'' +
+            ", name='" + name + '\'' +
+            '}';
+  }
+
+  public void setId(int id) {
+    this.id = id;
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/frst/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/frst/addressbook/tests/GroupCreationTests.java
@@ -1,10 +1,11 @@
 package ru.stqa.frst.addressbook.tests;
 
-        import org.testng.Assert;
-        import org.testng.annotations.Test;
-        import ru.stqa.frst.addressbook.model.GroupData;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.stqa.frst.addressbook.model.GroupData;
 
-        import java.util.List;
+import java.util.HashSet;
+import java.util.List;
 
 public class GroupCreationTests extends TestBase {
 
@@ -15,11 +16,22 @@ public class GroupCreationTests extends TestBase {
     app.gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", null,null));
+    GroupData group = new GroupData("test1", null, null);
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
+
+    int max = 0;
+    for(GroupData g : after){
+      if (g.getId() > max){
+        max = g.getId();
+      }
+    }
+group.setId(max);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
 }
