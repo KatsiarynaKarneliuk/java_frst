f14b8672d9a5fc370f35b60d2404ddadaf33ce2b
KatsiarynaKarneliuk <udivitelniymir@mail.ru>
2016-05-06 12:07:33 +0300

    Проверка на увеличение колличества элементов в списке после создания нового контакта

--- addressbook-web-tests/src/test/java/ru/stqa/frst/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/frst/addressbook/tests/ContactCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.frst.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.frst.addressbook.model.ContactData;
 
@@ -8,10 +9,12 @@ public class ContactCreationTests extends TestBase {
   @Test
   public void testContactCreation() {
     app.openHomePage();
+    int before = app.getContactHelper().getContactCount();
     app.getContactHelper().gotoAddNewContactPage();
     app.getContactHelper().fillContactForm(new ContactData("test1", "test2", null, "test1.test2@com", "265-45-89", "852-74-89","test1"), true);
     app.getContactHelper().submitAddNewContact();
     app.getContactHelper().returntoHome();
-
+    int after = app.getContactHelper().getContactCount();
+    Assert.assertEquals(after,before +1);
   }
 }
