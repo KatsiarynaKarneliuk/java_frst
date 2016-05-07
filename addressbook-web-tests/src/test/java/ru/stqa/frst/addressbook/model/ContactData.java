b06a3a3f267f09313d1529f562705dd67fc42796
KatsiarynaKarneliuk <udivitelniymir@mail.ru>
2016-05-07 19:50:18 +0300

    Сравнение списков целиком в ContactCeration c определением максимального идентификатора

--- addressbook-web-tests/src/test/java/ru/stqa/frst/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/frst/addressbook/model/ContactData.java
@@ -2,7 +2,7 @@ package ru.stqa.frst.addressbook.model;
 
 
 public class ContactData {
-  private final String id;
+  private int id;
   private final String name;
   private final String lastname;
   private final String address;
@@ -11,11 +11,11 @@ public class ContactData {
   private final String mobile;
   private static String group;
 
-  public String getId() {
+  public int getId() {
     return id;
   }
   public ContactData(String name, String lastname, String address, String email, String homephone, String mobile, String Group) {
-    this.id = null;
+    this.id = 0;
     this.name = name;
     this.lastname = lastname;
 
@@ -25,9 +25,32 @@ public class ContactData {
     this.mobile = mobile;
     group = Group;
   }
-  public ContactData(String id, String name, String lastname, String address, String email, String homephone, String mobile, String Group) {
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    ContactData that = (ContactData) o;
+
+    if (id != that.id) return false;
+    if (name != null ? !name.equals(that.name) : that.name != null) return false;
+    return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
+
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+    return result;
+  }
+
+  public ContactData(int id, String name, String lastname, String address, String email, String homephone, String mobile, String Group) {
     this.id = id;
     this.name = name;
+
     this.lastname = lastname;
 
     this.address = address;
@@ -68,32 +91,14 @@ public class ContactData {
   }
 
   @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    ContactData that = (ContactData) o;
-
-    if (id != null ? !id.equals(that.id) : that.id != null) return false;
-    if (name != null ? !name.equals(that.name) : that.name != null) return false;
-    return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
-
-  }
-
-  @Override
-  public int hashCode() {
-    int result = id != null ? id.hashCode() : 0;
-    result = 31 * result + (name != null ? name.hashCode() : 0);
-    result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
-    return result;
-  }
-
-  @Override
   public String toString() {
     return "ContactData{" +
             "id='" + id + '\'' +
             ", name='" + name + '\'' +
             ", lastname='" + lastname + '\'' +
             '}';
+  }
+
+  public void setId(int max) {
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/frst/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/frst/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.frst.addressbook.model.ContactData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
@@ -13,10 +14,22 @@ public class ContactCreationTests extends TestBase {
     app.openHomePage();
     List<ContactData>before = app.getContactHelper().getContactList();
     app.getContactHelper().gotoAddNewContactPage();
-    app.getContactHelper().fillContactForm(new ContactData("test1", "test2", null, "test1.test2@com", "265-45-89", "852-74-89","test1"), true);
+    ContactData contact = new ContactData("test1", "test2", null, "test1.test2@com", "265-45-89", "852-74-89","test1");
+    app.getContactHelper().fillContactForm(contact, true);
     app.getContactHelper().submitAddNewContact();
     app.getContactHelper().returntoHome();
     List<ContactData>after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(),before.size() +1);
+
+
+    int max = 0;
+    for (ContactData c : after){
+      if (c.getId()> max){
+        max =c.getId();
+      }
+    }
+    contact.setId(max);
+    before.add(contact);
+    Assert.assertEquals(new HashSet<Object>(after), new HashSet<Object>(before));
   }
 }
