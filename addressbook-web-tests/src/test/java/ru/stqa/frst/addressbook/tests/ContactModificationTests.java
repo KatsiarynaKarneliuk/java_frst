3bcd83299a1428def4bc4aba963dfd2d5aef2fcf
KatsiarynaKarneliuk <udivitelniymir@mail.ru>
2016-05-07 16:37:02 +0300

    Вопрос к 4,7 в ContactModification

--- addressbook-web-tests/src/test/java/ru/stqa/frst/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/frst/addressbook/tests/ContactModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.frst.addressbook.model.ContactData;
 
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -19,12 +20,16 @@ public class ContactModificationTests extends TestBase {
     List<ContactData>before = app.getContactHelper().getContactList();
     app.getContactHelper().selectContact(before.size() -1);
     app.getContactHelper().initContactModification();
-    app.getContactHelper().fillContactForm(new ContactData("test1", "test2", "test3", "test1.test2@com", "265-45-89", "852-74-89", null), false);
+    ContactData contact = new ContactData("test1", "test2", "test3", "test1.test2@com", "265-45-89", "852-74-89", null), false);
+    app.getContactHelper().fillContactForm(contact);
     app.getContactHelper().submitContactModification();
     app.getContactHelper().returntoHome();
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(),before.size());
 
+    before.remove(before.size() - 1);
+    before.add(contact);
+Assert.assertEquals(new HashSet<Object>(before),new HashSet<Object>(after));
   }
 }
 
