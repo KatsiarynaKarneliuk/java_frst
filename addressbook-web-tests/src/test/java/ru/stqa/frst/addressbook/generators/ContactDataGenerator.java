c0b742290b7cff12a2778325f64011e491f4ff85
KatsiarynaKarneliuk <udivitelniymir@mail.ru>
2016-05-17 10:44:35 +0300

    ContactDataGenerator

--- addressbook-web-tests/src/test/java/ru/stqa/frst/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/frst/addressbook/generators/ContactDataGenerator.java
@@ -3,6 +3,7 @@ package ru.stqa.frst.addressbook.generators;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
+import com.thoughtworks.xstream.XStream;
 import ru.stqa.frst.addressbook.model.ContactData;
 
 import java.io.File;
@@ -24,6 +25,9 @@ public class ContactDataGenerator {
   public String file;
 
 
+  @Parameter(names = "-d", description = "Data format")
+  public String format;
+
   public static void main(String[] args) throws IOException {
     ContactDataGenerator generator = new ContactDataGenerator();
     JCommander jCommander = new JCommander(generator);
@@ -38,25 +42,40 @@ public class ContactDataGenerator {
 
   private void run() throws IOException {
     List<ContactData> contacts = generateContacts(count);
-    save(contacts, new File(file));
+    if(format.equals("csv")) {
+      saveAsCsv(contacts, new File(file));
+    }else if(format.equals("xml")){
+      saveAsXml(contacts, new File (file));
+    }else{
+      System.out.println("unrecognized format" + format);
+    }
+  }
+
+  private void saveAsXml(List<ContactData> contacts, File file) throws IOException {
+    XStream xstream = new XStream();
+    xstream.processAnnotations(ContactData.class);
+    String xml = xstream.toXML (contacts);
+    Writer writer = new FileWriter(file);
+    writer.write(xml);
+    writer.close();
   }
 
   private List<ContactData> generateContacts(int count) {
     List<ContactData> contacts = new ArrayList<ContactData>();
     for (int i = 0; i < count; i++) {
-      contacts.add(new ContactData().withName(String.format("Иванов %s", i)).withLastname(String.format("test2 %s", i))
+      contacts.add(new ContactData().withName(String.format("Иван %s", i)).withLastname(String.format("Иванов %s", i))
               .withAddress(String.format("Петербург %s", i)).withEmail(String.format("test1.test2@com %s", i))
               .withEmail2(String.format("test1.test5@com %s", i)).withEmail3(String.format("test1.test10@com %s", i))
               .withHomephone(String.format("2654585 %s", i)).withMobile(String.format("8527485 %s", i))
-              .withWorkphone(String.format("1234575 %s", i)).withGroup(String.format("test1 %s", i)));
+              .withWorkphone(String.format("1234575 %s", i)).withGroup(String.format("test %s", i)));
     }
     return contacts;
   }
 
-  private void save(List<ContactData> contacts, File file) throws IOException {
+  private void saveAsCsv(List<ContactData> contacts, File file) throws IOException {
     Writer writer = new FileWriter(file);
     for (ContactData contact : contacts) {
-      writer.write(String.format("%s;%s;%s;\n", contact.getName(), contact.getLastname(), contact.getAddress()
+      writer.write(String.format("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n", contact.getName(), contact.getLastname(), contact.getAddress()
               , contact.getEmail(), contact.getEmail2(), contact.getEmail3(), contact.getHomephone(), contact.getMobile()
               , contact.getWorkphone(), contact.getGroup()));
     }
