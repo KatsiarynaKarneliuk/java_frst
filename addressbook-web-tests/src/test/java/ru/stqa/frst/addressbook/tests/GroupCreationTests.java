2592c906a8acf24bf351948d2dd1b088508b8f8e
KatsiarynaKarneliuk <udivitelniymir@mail.ru>
2016-05-19 21:42:18 +0300

    6.10/ 6.11

--- addressbook-web-tests/src/test/java/ru/stqa/frst/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/frst/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,8 @@ package ru.stqa.frst.addressbook.tests;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.frst.addressbook.model.ContactData;
@@ -26,37 +28,41 @@ public class GroupCreationTests extends TestBase {
 
   @DataProvider
   public Iterator<Object[]> validGroupsFromXml() throws IOException {
-      try(BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));)
-      {  String xml = "";
-    String line = reader.readLine();
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));) {
+      String xml = "";
+      String line = reader.readLine();
       while (line != null) {
-        xml+=line;
+        xml += line;
         line = reader.readLine();
       }
-    XStream xstream = new XStream();
-    xstream.processAnnotations(GroupData.class);
-    List<GroupData>groups=(List<GroupData>)xstream.fromXML(xml);
-   return groups.stream().map((g)-> new Object[]{g}).collect(Collectors.toList()).iterator();}
+      XStream xstream = new XStream();
+      xstream.processAnnotations(GroupData.class);
+      List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+      return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+    }
   }
 
   @DataProvider
   public Iterator<Object[]> validGroupsFromJson() throws IOException {
-    try(BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")));)
-    {String json = "";
-    String line = reader.readLine();
-    while (line != null) {
-     json+=line;
-      line = reader.readLine();
-    }
-   Gson gson = new Gson();
-    List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.json")));) {
+      String json = "";
+      String line = reader.readLine();
+      while (line != null) {
+        json += line;
+        line = reader.readLine();
+      }
+      Gson gson = new Gson();
+      List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>() {
+      }.getType());
 
-    return groups.stream().map((g)-> new Object[]{g}).collect(Collectors.toList()).iterator();}
+      return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+    }
   }
 
   @Test(dataProvider = "validGroupsFromJson")
 
   public void testGroupCreation(GroupData group) {
+
     app.goTo().groupPage();
     Groups before = app.group().all();
     app.group().create(group);
@@ -64,6 +70,6 @@ public class GroupCreationTests extends TestBase {
     Groups after = app.group().all();
     assertThat(after, equalTo(
             before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
-  }
 
+  }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/frst/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/frst/addressbook/tests/TestBase.java
@@ -1,27 +1,43 @@
 package ru.stqa.frst.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.frst.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by user on 18.04.2016.
  */
 public class TestBase {
-
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
   public void setUp() throws Exception {
     app.init();
   }
 
-  @AfterSuite
+  @AfterSuite(alwaysRun = true)
   public void tearDown() {
     app.stop();
   }
 
+
+  @BeforeMethod
+  public void logTestStart(Method m, Object[] p) {
+    logger.info("Star test " + m.getName() + "with paarameters"+ Arrays.asList(p));
+  }
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method m){
+    logger.info("Stop test"+ m.getName());
+  }
 }
