b0b0d24af39bdc22e289b1828203980e9f7ea283
KatsiarynaKarneliuk <udivitelniymir@mail.ru>
2016-06-08 22:44:58 +0300

    9.3 Созданы тесты GetProject, CreateIssue

---
+++ mantis-tests/src/test/java/ru/stqa/frst/mantis/tests/SoapTests.java
@@ -0,0 +1,36 @@
+package ru.stqa.frst.mantis.tests;
+
+        import org.testng.annotations.Test;
+        import ru.stqa.frst.mantis.model.Issue;
+        import ru.stqa.frst.mantis.model.Project;
+
+        import javax.xml.rpc.ServiceException;
+        import java.net.MalformedURLException;
+        import java.net.URL;
+        import java.rmi.RemoteException;
+        import java.util.Set;
+
+        import static org.testng.Assert.assertEquals;
+
+/**
+ * Created by user on 07.06.2016.
+ */
+public class SoapTests extends TestBase{
+  @Test
+  public  void testGetProjects() throws RemoteException, MalformedURLException, ServiceException {
+    Set<Project> projects = app.soap().getProjects();
+    System.out.println(projects.size());
+    for(Project project :projects)
+      System.out.println(project.getName());
+
+  }
+  @Test
+  public void testCreateIssue()throws RemoteException, MalformedURLException, ServiceException {
+    Set<Project> projects = app.soap().getProjects();
+  Issue issue = new Issue().withSummary("Test issue").withDescription("Test issue description")
+            .withProject(projects.iterator().next());
+    Issue created = app.soap().addIssue(issue);
+    assertEquals(issue.getSummary(), created.getSummary());
+  }
+}
+
