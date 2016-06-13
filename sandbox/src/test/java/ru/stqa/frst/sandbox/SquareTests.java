fed7afee834c7ed476f69b99d5dc0bc159b14d07
KatsiarynaKarneliuk <udivitelniymir@mail.ru>
2016-06-13 17:27:19 +0300

    Добавка к испорченному тесту

--- sandbox/src/test/java/ru/stqa/frst/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/frst/sandbox/SquareTests.java
@@ -1,7 +1,15 @@
 package ru.stqa.frst.sandbox;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 /**
  * Created by user on 13.06.2016.
  */
 public class SquareTests {
+  @Test
+  public void testArea(){
+    Square s = new Square(5);
+    Assert.assertEquals(s.area(), 20.0);
+  }
 }
